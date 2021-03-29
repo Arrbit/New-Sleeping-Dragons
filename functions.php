63e71f6a85b6ef03e0d35c88f85d2f7130df277a
Larissa Süßenbach <larissa.suessenbach@student.uni-siegen.de>
2021-03-29 14:24:04 +0200

    added bootstrap

--- functions.php
+++ functions.php
@@ -1,6 +1,14 @@
 <?php
 function enqueue_styles() {
     wp_enqueue_style('frontpage', get_template_directory_uri() . '/css/frontpage.css');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles');
+
+wp_enqueue_script(
+    'bootstrap',
+    get_template_directory_uri() . '/js/bootstrap.min.js',
+    array( 'jquery' ),
+);
+
 ?>

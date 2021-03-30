82a0accdd09ff1694a7e7f12119fc8813b931a5e
asfalfa <glkin9@gmail.com>
2021-03-30 13:54:34 +0200

    start of header

--- functions.php
+++ functions.php
@@ -25,14 +25,24 @@ function register_my_menus() {
 #Same for the widgets, just the init is here :)
 function widget_init() {
     if ( function_exists('register_sidebar') )
-      register_sidebar(array(
-        'name' => 'footer',
+    
+    register_sidebar(
+      array(
+        'name' => 'Footer',
         'before_widget' => '<div class = "footer">',
         'after_widget' => '</div>',
         'before_title' => '',
         'after_title' => '',
-      )
-    );
+      ));
+
+    register_sidebar(
+      array(
+        'name' => 'Header',
+        'before_widget' => '<div class = "header">',
+        'after_widget' => '</div>',
+        'before_title' => '',
+        'after_title' => '',
+      ));
 
     }
     add_action( 'widgets_init', 'widget_init' );
--- header.php
+++ header.php
@@ -8,3 +8,44 @@
 
 <!-- This container class will be closed in the footer :) -->
 <div class="container">
+
+<header>
+    <div class="p-2 d-flex" style="background-color: rgba(0, 0, 0, 0.2);">
+    
+        <div class="justify-content-start align-self-center color--black-accent">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("header") ) : endif; ?>
+        </div>
+
+        <div class="flex-fill justify-content-end align-self-center float-right">
+            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+        </div>
+
+    </div>
+</header>
+
+<style>
+#menu-header{
+    margin-top: auto;
+    margin-bottom: auto;
+}
+#menu-header > li {
+list-style:none;
+display: inline;
+padding-right:20px;
+}
+
+.color--black-accent{
+    color: #3e3e3e;
+
+}
+
+a {
+    color: #3e3e3e;
+    text-decoration: none;
+}
+
+a:hover {
+    color: black;
+    text-decoration: none;
+}
+</style>

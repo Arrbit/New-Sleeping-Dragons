bbd645c508802343e6483ada2dd360a855f6c55f
asfalfa <glkin9@gmail.com>
2021-03-31 18:34:27 +0200

    removed svg, added bkg container

--- front-page.php
+++ front-page.php
@@ -4,7 +4,59 @@
  * Theme Name: New Sleeping Dragons
  * Template Name: Frontpage
  */?>
-<?php get_header();?>
-frontpage
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<?php wp_head(); ?>
+</head>
+
+<div>
+<!-- Closes in Footer -->
+
+
+<div id="background-container">
+
+    <div>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logos/VectorRed_Transparent.png" class="frontpage-logo position-absolute" alt="SDS-Logo">
+    </div>
+
+    <div class="testSquare"></div>
+
+<div id="pusher"></div>
+
 <?php get_footer();?>
 
+<style>
+
+    .frontpage-logo {
+        width: 650px;
+        height: 650px;
+        transform: scaleX(-1);
+        margin-top: 20vh;
+        margin-left: -250px;
+    }
+
+    #background-container {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        /* test color */
+        background-color: red;
+    }
+    .testSquare {
+        width:100px;
+        height:100px;
+        color:white;
+        background-color:black;
+        
+    }
+
+    #pusher {
+        width: 100vw;
+        height: calc(100vh - 40px);
+        overflow: hidden;
+    }
+
+</style>

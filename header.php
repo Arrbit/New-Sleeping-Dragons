01899769a475c632e4cdb5386d4df3f53ac56843
asfalfa <glkin9@gmail.com>
2021-04-02 18:22:39 +0200

    switched to appbar setup

--- header.php
+++ header.php
@@ -6,24 +6,33 @@
 <?php wp_head(); ?>
 </head>
 
+<header id="sm-header" class="d-flex d-md-none appbar flex-row ">
+    <img class="appbar_mobile_button" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logos/VectorRed_White.png" class="logo" alt="SDS-Logo">
 
-<header class="wrap">
+    <div class="nav-menu-container appbar_menu">
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    </div>
+</header>
+
+<header id="md_header" class=" d-md-flex d-none wrap">
     <div class="links">
 
-        <div class="title-container flexi font--righteous">
+        <div class="title-container flexi font--righteous ">
         <?php echo get_bloginfo()?>
         </div>
 
-        <div class="logo-container flexi">
+        <div class="logo-container flexi d-xs-none">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logos/VectorRed_White.png"
                 class="logo" alt="SDS-Logo">
         </div>
 
-        <div class="nav-menu-container flexi flex-md-row flex-sm-column d-flex d-md-flex d-none">
+        <div class="nav-menu-container flexi flex-md-row">
             <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
         </div>
 
-        <div class="mobile-menu-container flex-row justify-content-between d-md-none w-100">
+
+        <!-- <div class="mobile-menu-container flex-row justify-content-between d-md-none w-100"> -->
+<!--         
                 <ul>
                     <li class="flex-fill rounded"><a href="/?page_id=29">Events</a></li>
                     <li class="flex-fill rounded"><a href="/?page_id=30">Letters</a></li>
@@ -31,7 +40,8 @@
                     <li class="flex-fill rounded"><a href="/?page_id=32">Guides</a></li>
                     <li class="flex-fill rounded"><a href="https://discord.gg/">Discord</a></li>
                 </ul>
-        </div>
+        </div> -->
+
         
     </div>
 </header>
@@ -40,6 +50,40 @@
 <div class="container">
 
 <style>
+.appbar_menu ul{
+    height:inherit;
+    padding:5px;
+    display: flex;
+    flex-wrap:wrap;
+    line-height:2em;
+}
+.appbar_menu li{
+    flex: 1 1 0px;
+    padding-right: unset !important;
+
+}
+
+.appbar a{
+    flex: auto; 
+    padding:5px;
+    border: 1px solid #D0D0D0;
+    text-align: center;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center
+
+}
+
+.appbar{
+    max-height:12vh;
+}
+
+.appbar_mobile_button{
+    width:auto;
+    min-height:40px;
+    max-height: inherit;
+}
 
 /* Big DIVs */
 .wrap {

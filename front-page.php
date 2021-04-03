faa33f044bd3ea1450a65a20b5492b735299cd20
asfalfa <glkin9@gmail.com>
2021-04-03 17:56:32 +0200

    added menu, tried new triangle method

--- front-page.php
+++ front-page.php
@@ -15,32 +15,43 @@
 <div>
 <!-- Closes in Footer -->
 
-
-<div id="background-container">
+<div id="frontpage--eyecatcher-container">
 
     <div>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logos/VectorRed_Transparent.png" class="frontpage-logo position-absolute" alt="SDS-Logo">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logos/VectorRed_Transparent.png"
+        id="frontpage--logo" class="position-absolute" alt="SDS-Logo">
     </div>
 
+    <div class="redTriangle"></div>
+    <div class="blackTriangle"></div>
     <div class="greyTriangle"></div>
 
 
+    <div id="frontpage--nav-menu-container" class="flex-column">
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    </div>
+
+</div>
+
+
 <?php get_footer();?>
-<script>
+
+<!-- <script>
 var el = jQuery(".greyTriangle");
 var w = screen.width / 4 | 0; // calculate & trim decimals
-el.css("border-width", "575px " + w + "px " + "575px 0 "); 
+el.css("border-width", "575px " + w + "px " + "575px 0 ");
+
 
 window.addEventListener('resize', change);
 function change(){
     var w = screen.width / 4 | 0; // calculate & trim decimals
     el.css("border-width", "575px " + w + "px " + "575px 0 ");
 };
-</script>
+</script> -->
 
 <style>
 
-    .frontpage-logo {
+    #frontpage--logo {
         width: 650px;
         height: 650px;
         transform: scaleX(-1);
@@ -48,18 +59,44 @@ function change(){
         margin-left: -250px;
     }
 
-    #background-container {
+    #frontpage--eyecatcher-container {
         max-width: 100vw;
         height: 100vh;
         position: relative;
         /* test color */
-        background-color: grey;
+        background-color: white
     }
+
+    #frontpage--nav-menu-container {
+        transform: translate(50vw, 50vh);
+        position: absolute;
+    }
+
+    #frontpage--nav-menu-container  li {
+        list-style:none;
+        padding-right:20px;
+        position: relative;
+        margin-bottom: 5px;
+    }
+    #frontpage--nav-menu-container  li a{
+        flex: auto; 
+        padding:5px;
+        border: 0px solid #3E3E3E;
+        border-radius: 5px;
+        color: #8A0707;
+        background-color: rgba(208, 208, 208, 0.7);
+        text-align: center;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        justify-content: center
+    }
+
     .greyTriangle {
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 575px 1400px 575px 0;
+        border-width: 55vh 50vw 55vh 0; /* Old Values : 575px 1400px 575px 0; */
         border-color: transparent #D0D0D0 transparent;
         position: absolute;
         top: 0;
@@ -71,24 +108,21 @@ function change(){
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 575px 1400px 575px 0;
-        border-color: transparent #D0D0D0 transparent;
+        border-width: 20vw 20vh 0 20vh;
+        border-color:  #8A0707 transparent transparent;
         position: absolute;
-        top: 0;
-        right: 0;
-        margin-top: -50px;
+        top: 40px;
+        right: 40px;
     }
 
     .blackTriangle {
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 575px 1400px 575px 0;
-        border-color: transparent #D0D0D0 transparent;
+        border-width: 20vw 20vh 0 20vh;
+        border-color: #3E3E3E transparent transparent;
         position: absolute;
-        top: 0;
-        right: 0;
-        margin-top: -50px;
+        right: 30px;
     }
 
 </style>

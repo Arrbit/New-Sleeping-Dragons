da6cfd73d25db5c2da9aa3836baaf7f091866228
Larissa Süßenbach <larissa.suessenbach@student.uni-siegen.de>
2021-04-04 19:47:29 +0200

    404 start

--- 404.php
+++ 404.php
@@ -4,5 +4,20 @@
  * Theme Name: New Sleeping Dragons
  */ ?>
 <?php get_header();?>
-404
+
+<div class="card card_container flex-column d-flex super-round">
+<div class="card_img member_card_img">
+<img sizes="(max-width: 768px) 100vw, 768px"  width="768" height="432"   loading="lazy" 
+                         class="attachment-medium_large size-medium_large wp-post-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/404.png">
+                         </div>
+
+    <div class="index_content">
+        <div class="card_title font--righteous">
+        Error 404 Not Found
+        </div>
+        You are lost. We also do not know how you got here, so please go on our discord and complain. Thank you :D
+    </div>
+</div>
+
+
 <?php get_footer();?>

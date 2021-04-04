eafa90747baa56edb86de65fb4575f02ee94445b
asfalfa <glkin9@gmail.com>
2021-04-04 19:17:51 +0200

    General Adjustments

--- front-page.php
+++ front-page.php
@@ -28,21 +28,24 @@
     <div id="vLine"></div>
     <div id="hLine"></div>
 
-    <!-- <div id="eyecatcher--title-container" class="font--righteous">
+    <div id="eyecatcher--title-container" class="font--righteous">
         <div class="position-absolute">
             <?php echo get_bloginfo()?>
-             Sleeping<br>Dragons (alternative)
+             <!--Sleeping<br>Dragons (alternative)-->
         </div>
-    </div> -->
+    </div>
 
-    <div id="frontpage--nav-menu-container" class="flex-column">
+    <div id="frontpage--nav-menu-container" class="flex-row">
         <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
     </div>
 
-    <!-- <div id="eyecatcher--button-container">
+    <div id="eyecatcher--button-container">
         <div id="eyecatcher-button-text" class="position-absolute">A Phoenix-based FC.</div>
         <a class="position-absolute" href="#">EXPLORE</a>
-    </div>  -->
+        <div id="lead-arrow-2"></div>
+        <div id="lead-arrow"></div>
+        <hr role="separator">
+    </div>
 
 </div>
 
@@ -74,64 +77,80 @@ function change(){
 
     /* Logo */
     #frontpage--logo {
-        width: 30vw;
-        height: 50vh;
+        width: 32vw;
+        height: 55vh;
         transform: scaleX(-1);
         margin-top: 25vh;
         left:-15vw;
     }
 
     /* Title */
-    #eyecatcher--title-container{
-        transform: translate(18vw, 30vh);
+    #eyecatcher--title-container div{
+        transform: translate(17vw, 30vh);
         font-size: 500%;
     }
 
     /* Nav Menu */
     #frontpage--nav-menu-container {
-        transform: translate(69vw, 52vh);
         position: absolute;
+        transform: translate(25vw, 45vh);
     }
     #frontpage--nav-menu-container  li {
         list-style:none;
         padding-right:20px;
         position: relative;
+        display: inline;
         margin-bottom: 5px;
+        
     }
     #frontpage--nav-menu-container  li a{
         flex: auto; 
-        padding:5px;
+        padding: 10px 20px 10px 20px;
         border: 1px solid rgba(62, 62, 62, 0.4);
         border-radius: 3px;
         color: #8A0707;
         background-color: rgba(208, 208, 208, 0.7);
         text-align: center;
         text-decoration: none;
-        display: flex;
         align-items: center;
         justify-content: center;
     }
 
     /* Explore Button */
-    #eyecatcher--button-container{
-        transform: translate(calc(50vw - 45px), 70vh);
-    }
     #eyecatcher-button-text {
-        transform: translate(-15px, -25px);
+        transform: translate(calc(50vw - 62px),calc(90vh - 25px));
     }
     #eyecatcher--button-container a{
         flex: auto; 
         padding:7px 20px 7px 20px;
-        border: 2px solid rgba(62, 62, 62, 1);
+        border: 1px solid rgba(62, 62, 62, 1);
         border-radius: 5px;
         color: rgba(62, 62, 62, 1);
         background-color: rgba(208, 208, 208, 0.7);
         text-align: center;
         text-decoration: none;
-        font-weight: bold;
         display: flex;
         align-items: center;
         justify-content: center;
+        transform: translate(calc(50vw - 45px), 90vh);
+    }
+    #lead-arrow {
+        border: solid rgba(62, 62, 62, 1);
+        border-width: 0 3px 3px 0;
+        position: absolute;
+        padding: 6px;
+        transform: translate(calc(50vw + 3px), calc(90vh + 45px)) rotate(45deg);
+    }
+    #lead-arrow-2 {
+        border: solid rgba(62, 62, 62, 1);
+        border-width: 0 3px 3px 0;
+        position: absolute;
+        padding: 3px;
+        transform: translate(calc(50vw + 5.5px), calc(90vh + 43px)) rotate(45deg);
+    }
+    #eyecatcher--button-container hr{
+        transform: translate(25vw,96vh);
+        width: 50%;
     }
 
     /* Shapes */
@@ -139,7 +158,7 @@ function change(){
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 55vh 50vw 55vh 0; /* Old Values : 575px 1400px 575px 0; */
+        border-width: 50vh 35vw 50vh 0; /* Old Values : 575px 1400px 575px 0; */
         border-color: transparent #D0D0D0 transparent;
         position: absolute;
         top: 0;
@@ -150,7 +169,7 @@ function change(){
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 40vw 20vh 0 40vh;
+        border-width: 40vw 15vh 0 40vh;
         border-color:  #8A0707 transparent transparent;
         position: absolute;
         top: 6%;
@@ -160,25 +179,25 @@ function change(){
         width: 0;
         height: 0;
         border-style: solid;
-        border-width: 40vw 20vh 0 40vh;
+        border-width: 40vw 15vh 0 40vh;
         border-color: #3E3E3E transparent transparent;
         position: absolute;
         right: calc(3% - 1vw);
         max-width: 10vw;
     }
-    #vLine{
+    /* #vLine{
         border-left: 3px solid #3E3E3E;
-        height: 50%;
+        height: 38vh;
         position: absolute;
-        top: 20vh;
-        right: 23vw;
+        top: 28vh;
+        right: 20vw;
     }
     #hLine{
         border-top: 3px solid #3E3E3E;
-        width: 30%;
+        width: 20vw;
         position: absolute;
-        top: 50vh;
-        right: 5vw;
-    }
+        top: 45vh;
+        right:8vw;
+    } */
 
 </style>

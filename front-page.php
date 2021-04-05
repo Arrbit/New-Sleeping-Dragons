51e69f1548d0384a8f27d80bdda6cd974ffcad95
Larissa Süßenbach <larissa.suessenbach@student.uni-siegen.de>
2021-04-05 20:40:09 +0200

    event php frontpage setup

--- front-page.php
+++ front-page.php
@@ -47,4 +47,75 @@
     </div>
 
 
+ <!-- If there is no event, dont display this, you need to check for 2 null querys >:( -->
+ <div class="card_collection">
+
+
+    <?php
+$args = array(
+    'meta_key'          => 'date',
+    'orderby'           => 'meta_value',
+    'order'             => 'ASC',
+    'post_type'        => 'form_events', // the post type 
+    'post_status' => 'publish',
+    'meta_query' => array( // WordPress has all the results, now, return only the events after today's date
+        array(
+            'key' => 'date', // Check the start date field
+            'value' => date("Y-m-d"), // Set today's date (note the similar format)
+            'compare' => '==', // Return the ones greater than today's date
+            'type' => 'DATE' // Let WordPress know we're working with date
+        )
+    ),
+);
+
+$loop = new WP_Query( $args );
+
+if( ($loop->have_posts())) { ?>  
+  <h3 class="our_events_today"> Our Events today: </h3> 
+  <?php
+}  ## This checks if the query above is empty or not :)
+
+while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+<div class="card card_container flex-column d-flex super-round">
+    <?php if ( has_post_thumbnail() ) : ?>
+        <div class="card_img member_card_img">
+        <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+        <a target="_blank" href="<?php echo $url ?>" title="<?php the_title_attribute(); ?>">
+            <?php the_post_thumbnail("large"); ?>
+        </a>
+        </div>
+    <?php endif; ?>
+
+    <div class="index_content">
+        <div class="card_title event_title font--righteous">
+            <?php the_title(); ?>
+        </div>
+        <div class="card_time">
+            <?php echo get_post_meta(get_the_ID(), 'hh', TRUE); ?>:<?php echo get_post_meta(get_the_ID(), 'mm', TRUE); ?>ST on <?php echo date('l', strtotime(get_post_meta(get_the_ID(), 'date', TRUE))); ?> <?php echo date('d.m.Y',strtotime(get_post_meta(get_the_ID(), 'date', TRUE))); ?>
+        </div>
+        <?php the_content();?>
+        <div class="event_author"> by <?php echo get_post_meta(get_the_ID(), 'your_name', TRUE);?> </div>
+    </div>
+</div>
+
+            
+                
+<?php endwhile; ?>
+<?php wp_reset_query(); ?>
+
+<!-- Add addtional query if above if null for the next event -->
+
+
+
+</div>
+
+
 <?php get_footer();?>
+
+
+<style>
+.card_collection{
+    max-width:500px;
+}
+</style>
